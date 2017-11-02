729267ef98f03d254db22bdb5d9e741933c60515
zaidbhimani <zaidbhimani786@gmail.com>
2017-11-02 14:56:00 +0530

    new-folder

---
+++ trying javascript/custom.js
@@ -0,0 +1,16 @@
+$(document).ready(function(){
+
+document.getElementById('but1').addEventListener("click", function(){
+
+	document.getElementById('box').style.backgroundColor="green";
+});
+
+document.getElementById('but2').addEventListener("click", function(){
+
+	document.getElementById('box').style.backgroundColor="red";
+});
+
+
+
+
+});
