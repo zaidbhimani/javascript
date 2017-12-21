686db86708e4d09bad60624b4a20d574a132afcd
shoaibbhimani <shoaibbhimani1392@gmail.com>
2017-12-22 01:26:20 +0530

    'completed-compare-picture-app'

--- trying javascript/compare picture image using javascript/custom.js
+++ trying javascript/compare picture image using javascript/custom.js
@@ -19,19 +19,39 @@ var imagebox = [
 }
 ]
 
-var indexNum = 0;
+var indexNum = -1;
 
-var indexNum2 = 0;
+var indexNum2 = -1;
 
 var result = '';
 var result2 = '';
 
 document.getElementById('divResult').addEventListener('click', function(){
+   // let result = '';
+   //Increment
+   indexNum++;
+
+   //Index should not be more than length
+   if( indexNum > imagebox.length - 1){
+     result = '';
+     return $('#divResulttwo').html(result);
+   }
+
+   // compare two number if they are same increment one more time 
+  if (indexNum == indexNum2){
+     indexNum++;
+  } 
    
+  result = 'Name:-' + imagebox[indexNum].name +'</br>'+'age:-' + imagebox[indexNum].age;
+
+   $('#divResulttwo').html(result);
+
+
+
+/*
     if (indexNum == indexNum2){
-    indexNum++;
-     result = 'Name:-' + imagebox[indexNum].name +'</br>'+'age:-' + imagebox[indexNum].age;
-       $('#divResulttwo').html(result);
+    
+       
    }
     else if( indexNum < 4){
     result = 'Name:-' + imagebox[indexNum].name +'</br>'+'age:-' + imagebox[indexNum].age;
@@ -42,23 +62,31 @@ document.getElementById('divResult').addEventListener('click', function(){
    else{
       result = '';
       $('#divResulttwo').html(result);
-   }
+   }*/
 
 });
 
 document.getElementById('divResulttwo').addEventListener('click', function(){
-    if( indexNum2 < 4){
-     result2 = 'Name:-' + imagebox[indexNum2].name +'</br>'+'age:-' + imagebox[indexNum2].age;
-   $('#divResult').html(result2);
-    indexNum2++;
-  }
-  else{
-     result2 = '';
-      $('#divResult').html(result2);
-  }
-   //  if(indexNum == indexNum2){
-   //  indexNum2++;
-   // }
+   // let result = '';
+   //Increment
+   indexNum2++;
+
+   //Index should not be more than length
+   if( indexNum2 > imagebox.length - 1){
+     result = '';
+     return $('#divResult').html(result);
+   }
+
+   // compare two number if they are same increment one more time 
+  if (indexNum == indexNum2){
+     indexNum2++;
+  } 
+   
+  result = 'Name:-' + imagebox[indexNum2].name +'</br>'+'age:-' + imagebox[indexNum2].age;
+
+   $('#divResult').html(result);
+
+
 
 });
 
